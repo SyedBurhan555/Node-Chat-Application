5607bbc05208cd9be9b20df4b9943dcddee5406d
Syed Burhan <syedburhanali555@gmail.com>
2023-06-07 17:55:17 +0500

    feat/add-socket-io-and-create-listners

--- NodeServers/index.js
+++ NodeServers/index.js
@@ -1,1 +1,18 @@
-console.log("heelo world");
+const io = require("socket.io")(8000);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+    console.log("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
